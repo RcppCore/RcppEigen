f8cb9f278493ee5e42704957b366da40a99e5ce9
Douglas Bates <dmbates@gmail.com>
2011-06-15 19:06:59 +0000

    Add support for sparse Cholesky and LU

--- inst/include/RcppEigen.h
+++ inst/include/RcppEigen.h
@@ -22,10 +22,12 @@
 #ifndef RcppEigen__RcppEigen__h
 #define RcppEigen__RcppEigen__h
 
+#define EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET
 #include <RcppEigenForward.h>
 #include <Rcpp.h>
 #include <RcppEigenWrap.h>
 
 #include <Eigen/Dense>
+#include <unsupported/Eigen/SparseExtra> // also includes Eigen/Sparse
 
 #endif
