156726a26028c9e4f9495f30d1d1dcafc58c15a9
Douglas Bates <dmbates@gmail.com>
2011-06-15 19:07:37 +0000

    Add support for inline package

---
+++ R/inline.R
@@ -0,0 +1,22 @@
+## Copyright (C)       2010 Dirk Eddelbuettel, Romain Francois and Douglas Bates
+##
+## This file is part of RcppEigen.
+##
+## RcppEigen is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 of the License, or
+## (at your option) any later version.
+##
+## RcppEigen is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with RcppEigen.  If not, see <http://www.gnu.org/licenses/>.
+
+inlineCxxPlugin <- Rcpp:::Rcpp.plugin.maker(
+	include.before = "#include <RcppEigen.h>", 
+	package        = "RcppEigen"
+)
+
