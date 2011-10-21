bcc4135df82492c4591c55501f110006c72b23a1
Douglas Bates <dmbates@gmail.com>
2011-10-21 22:49:03 +0000

    Clean up code.

--- src/fastLm.h
+++ src/fastLm.h
@@ -23,104 +23,84 @@
 #define RCPPEIGEN_FASTLM_H
 
 #include <RcppEigen.h>
-				// Basic matrix, vector and array types for double
-typedef Eigen::MatrixXd                                MatrixXd;
-typedef Eigen::VectorXd                                VectorXd;
-typedef Eigen::ArrayXd                                 ArrayXd;
-typedef Eigen::ArrayXXd                                ArrayXXd;
-				// integer
-typedef Eigen::MatrixXi                                MatrixXi;
-typedef Eigen::VectorXi                                VectorXi;
-typedef Eigen::ArrayXi                                 ArrayXi;
-typedef Eigen::ArrayXXi                                ArrayXXi;
-				// complex
-typedef Eigen::MatrixXcd                               MatrixXcd;
-typedef Eigen::VectorXcd                               VectorXcd;
-typedef Eigen::ArrayXcd                                ArrayXcd;
-typedef Eigen::ArrayXXcd                               ArrayXXcd;
-				// these should be defined for each base type - we use double
-typedef MatrixXd::Index                                Index;
-typedef MatrixXd::Scalar                               Scalar;
-typedef MatrixXd::RealScalar                           RealScalar;
-				// Mapped matrix and vector types (share storage)
-typedef Eigen::Map<MatrixXd>                           MMatrixXd;
-typedef Eigen::Map<VectorXd>                           MVectorXd;
-				// Views
-typedef Eigen::TriangularView<MatrixXd, Eigen::Upper>  UpperTri;
-typedef Eigen::TriangularView<MatrixXd, Eigen::Lower>  LowerTri;
-typedef Eigen::SelfAdjointView<MatrixXd, Eigen::Upper> UpperSym;
-typedef Eigen::SelfAdjointView<MatrixXd, Eigen::Lower> LowerSym;
-				// Decomposition types
-typedef Eigen::LLT<MatrixXd>                           LLTType;
-typedef Eigen::LDLT<MatrixXd>                          LDLTType;
-typedef Eigen::ColPivHouseholderQR<MatrixXd>           PivQRType;
-typedef Eigen::HouseholderQR<MatrixXd>                 QRType;
-typedef Eigen::JacobiSVD<MatrixXd>                     SVDType;
-typedef Eigen::DiagonalMatrix<double, Eigen::Dynamic>  DiagType;
-typedef Eigen::SelfAdjointEigenSolver<MatrixXd>        EigvType;
-				// Types derived from decompositions
-typedef PivQRType::PermutationType                     PermutationType;
-typedef PermutationType::IndicesType                   IndicesType;
 
-class lm {
-protected:
-    Index        m_n;	   /**< number of rows */
-    Index        m_p;	   /**< number of columns */
-    VectorXd     m_coef;   /**< coefficient vector */
-    int          m_r;	   /**< computed rank or NA_INTEGER */
-    int          m_df;	   /**< residual degrees of freedom */
-    IndicesType  m_perm;   /**< column permutation */
-    VectorXd     m_fitted; /**< vector of fitted values */
-    MatrixXd     m_unsc;   /**< unscaled variance-covariance matrix */
-    RealScalar   m_prescribedThreshold;	/**< user specified tolerance */
-    bool         m_usePrescribedThreshold;
-public:
-    lm(const MMatrixXd&, const MVectorXd&);
-    lm&        setThreshold(const RealScalar&); // patterned after ColPivHouseholderQR
-    RealScalar    threshold() const;
-    const VectorXd&    coef() const {return m_coef;}
-    int                rank() const {return m_r;}
-    int                  df() const {return m_df;}
-    const IndicesType& perm() const {return m_perm;}
-    const VectorXd&  fitted() const {return m_fitted;}
-    const MatrixXd&    unsc() const {return m_unsc;}
-};
+namespace lmsol {
+    using Eigen::ArrayXd;
+    using Eigen::ColPivHouseholderQR;
+    using Eigen::HouseholderQR;
+    using Eigen::JacobiSVD;
+    using Eigen::LDLT;
+    using Eigen::LLT;
+    using Eigen::Lower;
+    using Eigen::Map;
+    using Eigen::MatrixXd;
+    using Eigen::SelfAdjointEigenSolver;
+    using Eigen::TriangularView;
+    using Eigen::VectorXd;
+    using Eigen::Upper;
 
-class ColPivQR : public lm {
-public:
-    ColPivQR(const MMatrixXd&, const MVectorXd&);
-};
+    typedef MatrixXd::Index                                 Index;
+    typedef MatrixXd::Scalar                                Scalar;
+    typedef MatrixXd::RealScalar                            RealScalar;
+    typedef ColPivHouseholderQR<MatrixXd>::PermutationType  Permutation;
+    typedef Permutation::IndicesType                        Indices;
 
-class LLT : public lm {
-public:
-    LLT(const MMatrixXd&, const MVectorXd&);
-};
+    class lm {
+    protected:
+	Index      m_n;		/**< number of rows */
+	Index      m_p;		/**< number of columns */
+	VectorXd   m_coef;	/**< coefficient vector */
+	int        m_r;		/**< computed rank or NA_INTEGER */
+	int        m_df;	/**< residual degrees of freedom */
+	Indices    m_perm;	/**< column permutation */
+	VectorXd   m_fitted;	/**< vector of fitted values */
+	MatrixXd   m_unsc;	/**< unscaled variance-covariance  */
+	RealScalar m_prescribedThreshold; /**< user specified tolerance */
+	bool       m_usePrescribedThreshold;
+    public:
+	lm(const Map<MatrixXd>&, const Map<VectorXd>&);
+	lm&        setThreshold(const RealScalar&); // patterned after ColPivHouseholderQR code
+	RealScalar    threshold() const;
+	const VectorXd&    coef() const {return m_coef;}
+	int                rank() const {return m_r;}
+	int                  df() const {return m_df;}
+	const Indices&     perm() const {return m_perm;}
+	const VectorXd&  fitted() const {return m_fitted;}
+	const MatrixXd&    unsc() const {return m_unsc;}
+    };
 
-class LDLT : public lm {
-public:
-    LDLT(const MMatrixXd&, const MVectorXd&);
-};
+    class ColPivQR : public lm {
+    public:
+	ColPivQR(const Map<MatrixXd>&, const Map<VectorXd>&);
+    };
 
-class QR : public lm {
-public:
-    QR(const MMatrixXd&, const MVectorXd&);
-};
+    class Llt : public lm {
+    public:
+	Llt(const Map<MatrixXd>&, const Map<VectorXd>&);
+    };
 
-class SVD : public lm {
-public:
-    SVD(const MMatrixXd&, const MVectorXd&);
-};
+    class Ldlt : public lm {
+    public:
+	Ldlt(const Map<MatrixXd>&, const Map<VectorXd>&); 
+    };
 
-class SymmEigen : public lm {
-public:
-    SymmEigen(const MMatrixXd&, const MVectorXd&);
-};
+    class QR : public lm {
+    public:
+	QR(const Map<MatrixXd>&, const Map<VectorXd>&);
+    };
+
+    class SVD : public lm {
+    public:
+	SVD(const Map<MatrixXd>&, const Map<VectorXd>&);
+    };
+
+    class SymmEigen : public lm {
+    public:
+	SymmEigen(const Map<MatrixXd>&, const Map<VectorXd>&);
+    };
+}
 
 extern "C" SEXP fastLm(SEXP Xs, SEXP ys, SEXP types);
-
-extern "C" SEXP crossprod(SEXP Xs);
-
-extern "C" SEXP tcrossprod(SEXP Xs);
 
 #endif
 
