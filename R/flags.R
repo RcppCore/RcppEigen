1798cb8b9fa5fb151cc80752b760b95f105feb46
Dirk Deelbuettel <edd@debian.org>
2012-08-15 03:07:12 +0000

    add CxxFlags() and RcppEigenCxxFlags() for use in Makefiles

---
+++ R/flags.R
@@ -0,0 +1,24 @@
+## Copyright (C)       2012  Douglas Bates, Dirk Eddelbuettel and Romain Francois
+##
+## This file is part of RcppEigen.
+##
+## RcppEigen is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 of the License, or
+## (at your option) any later version.
+##
+## RcppEigen is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with RcppEigen.  If not, see <http://www.gnu.org/licenses/>.
+
+RcppEigenCxxFlags <- function() {
+    paste('-I"', system.file("include", package="RcppEigen"), '"', sep="")
+}
+
+CxxFlags <- function() {
+    cat(RcppEigenCxxFlags())
+}
