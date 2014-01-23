fb97bf68639e70b96e270e6cd74d7821904ffa15
Dirk Eddelbuettel <edd@debian.org>
2014-01-22 21:35:23 -0600

    small changes forced upon the package by Rcpp

--- R/SHLIB.R
+++ R/SHLIB.R
@@ -15,8 +15,8 @@
 ## You should have received a copy of the GNU General Public License
 ## along with RcppEigen.  If not, see <http://www.gnu.org/licenses/>.
 
-SHLIB <- Rcpp:::SHLIB.maker( 
-    env = list( 
-        PKG_LIBS = Rcpp:::RcppLdFlags(),
-   )
-)
+#SHLIB <- Rcpp:::SHLIB.maker( 
+#    env = list( 
+#        PKG_LIBS = Rcpp:::RcppLdFlags(),
+#   )
+#)
