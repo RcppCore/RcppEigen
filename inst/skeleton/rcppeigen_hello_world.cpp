a7423cb0dc921dd85337e06686dbc0a91043d1e0
Jonah Gabry <jgabry@gmail.com>
2022-09-15 06:00:34 -0600

    rcppeigen_hello_world.cpp: fix typo in comments (#114)

--- inst/skeleton/rcppeigen_hello_world.cpp
+++ inst/skeleton/rcppeigen_hello_world.cpp
@@ -9,7 +9,7 @@
 // [[Rcpp::depends(RcppEigen)]]
 
 // simple example of creating two matrices and
-// returning the result of an operatioon on them
+// returning the result of an operation on them
 //
 // via the exports attribute we tell Rcpp to make this function
 // available from R
